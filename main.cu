873d7d9d2ff268ee01651e2ae294cdd40ea05461
Code5150 <f>
2021-10-01 21:50:40 +0400

    Переписана функция умножения матриц на GPU

--- main.cu
+++ main.cu
@@ -8,7 +8,10 @@
 #include "cpu_mmul.h"
 
 #define ULLCAST(X) static_cast<unsigned long long>(X)
+#define INTCAST(X) static_cast<int>(X)
 //#define LAB_DEBUG
+
+constexpr const int BLOCK_SIZE = 500;
 
 template<typename t>
 inline size_t malloc_size(size_t s) {
@@ -19,7 +22,7 @@ inline size_t malloc_size(size_t s) {
  * c = alpha*transa(a)*transb(b) + beta*c
  * [lda X ldb] * [ldb X ldc] = [lda X ldc]
  */
-void cuda_mmul(cublasHandle_t *chandle, float* c_buffer, float* a, float* b, float *c,
+/*void cublasMmul(cublasHandle_t *chandle, float* c_buffer, float* a, float* b, float *c,
                int lda, int ldb, int ldc, int m, int n, int k)  {
     float alpha = 1.0f;
     float beta = 0.0f;
@@ -46,6 +49,43 @@ void cuda_mmul(cublasHandle_t *chandle, float* c_buffer, float* a, float* b, flo
                 b, ldb,
                 c, ldc
                 );
+}*/
+
+//Considering matrix B as transposed
+__global__ void cudaMmul(const float* a, const float* b, float *c, int N, int matrix_size) {
+    unsigned int blockBegin = blockIdx.x * BLOCK_SIZE;// + blockIdx.y * N;
+    unsigned int blockShared = blockBegin + blockIdx.y * N;
+
+    __shared__ float aShared[BLOCK_SIZE];
+
+    //if(threadIdx.x == 0)printf("Block %d, %d begins from %d\n",blockIdx.x, blockIdx.y, blockBegin);
+    //Copy values from A block and initial values from
+    aShared[threadIdx.x] = a[blockShared + threadIdx.x];
+    __syncthreads();
+    float sum;
+    for(unsigned int i = blockBegin; i < matrix_size; i += N) {
+        sum = 0.0f;
+        sum += aShared[threadIdx.x] * b[i + threadIdx.x];
+        __syncthreads();
+        unsigned int ind = blockIdx.y*N + (i / N);
+        atomicAdd(&c[ind], sum);
+    }
+
+}
+
+void transpose(cublasHandle_t *chandle, float* result_buffer, float* b, float *result,
+               int ldb, int ldc, int m, int n) {
+    float alpha = 1.0f;
+    float beta = 0.0f;
+    cublasSgeam(*chandle,
+                CUBLAS_OP_T, CUBLAS_OP_N,
+                m, n,
+                &alpha,
+                result_buffer, ldc,
+                &beta,
+                b, ldb,
+                result, ldc
+    );
 }
 
 void verify_result(const float *a, const float *b, const float *c, int N) {
@@ -60,7 +100,7 @@ void verify_result(const float *a, const float *b, const float *c, int N) {
                 tmp += a[i * N + k] * b[k * N + j];
             }
             // Check against the CPU result
-            assert(tmp - c[i * N + j] < 1e-3);
+            //assert(tmp - c[i * N + j] < 1e-3);
             if(tmp - c[i * N + j] > 1e-3) {
                 printf("Verification failed. tmp - c[i * N + j] > 1e-3\n");
                 return;
@@ -77,24 +117,71 @@ void print_matrix(const float* matrix, const size_t N) {
         }
         printf("\n");
     }
+    printf("\n");
 }
 #endif
 
 void scan_matrix_dim(int* matrix_dim) {
     printf("Введите размер матрицы (от 100 до 2000): ");
     scanf_s("%d", matrix_dim);
-    while (*matrix_dim < 100 || *matrix_dim > 2000) {
+    while (*matrix_dim < 0 || *matrix_dim > 2000) {
         printf("Неверный размер матрицы. %d не входит в интервал [100;2000]\n", *matrix_dim);
         printf("Введите размер матрицы (от 100 до 2000): ");
         scanf_s("%d", matrix_dim);
     }
 }
 
+cudaDeviceProp showProperties() {
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, 0);//определение параметров GPU с номером 0
+
+    printf("Device name : %s\n", deviceProp.name);
+    printf("Total global memory : %llu MB\n",
+           deviceProp.totalGlobalMem / 1024 / 1024);
+    printf("Shared memory per block : %zu\n",
+           deviceProp.sharedMemPerBlock);
+    printf("Registers per block : %d\n",
+           deviceProp.regsPerBlock);
+    printf("Warp size : %d\n", deviceProp.warpSize);
+    printf("Memory pitch : %zu\n", deviceProp.memPitch);
+    printf("Max threads per block : %d\n",
+           deviceProp.maxThreadsPerBlock);
+    printf("Max threads dimensions : x = %d, y = %d, z = %d\n",
+           deviceProp.maxThreadsDim[0],
+           deviceProp.maxThreadsDim[1],
+           deviceProp.maxThreadsDim[2]);
+    printf("Max grid size: x = %d, y = %d, z = %d\n",
+           deviceProp.maxGridSize[0], deviceProp.maxGridSize[1],
+           deviceProp.maxGridSize[2]);
+    printf("Clock rate: %d\n", deviceProp.clockRate);
+    printf("Total constant memory: %zu\n",
+           deviceProp.totalConstMem);
+    printf("Compute capability: %d.%d\n",
+           deviceProp.major, deviceProp.minor);
+    printf("Texture alignment: %zu\n",
+           deviceProp.textureAlignment);
+    printf("Device overlap: %d\n",
+           deviceProp.deviceOverlap);
+    printf("Multiprocessor count: %d\n",
+           deviceProp.multiProcessorCount);
+    printf("Kernel execution timeout enabled: %s\n",
+           deviceProp.kernelExecTimeoutEnabled ? "true" :
+           "false");
+    printf("Can map host memory: %s\n",
+           deviceProp.canMapHostMemory ? "true" :
+           "false");
+    printf("Device has Compute Capability %d.%d\n",
+           deviceProp.major, deviceProp.minor);
+
+    return deviceProp;
+}
+
 enum Options {
     NONE,
     EXIT = 0,
     PAR_MUL_GPU = 1,
-    PAR_MUL_CPU = 2
+    PAR_MUL_CPU = 2,
+    DEVICE_INFO = 3
 };
 
 int main() {
@@ -104,7 +191,7 @@ int main() {
     cublasHandle_t HANDLE;
     cublasCreate_v2(&HANDLE);
 
-    float *a, *b, *c, *c_buffer;
+    float *a, *b, *c, *buffer;
     float *cpuA, *cpuB, *cpuC;
 
     curandCreateGeneratorHost(&gen, CURAND_RNG_PSEUDO_MTGP32);
@@ -118,18 +205,19 @@ int main() {
         printf("0 - выход из программы\n");
         printf("1 - умножение матриц на GPU\n");
         printf("2 - умножение матриц на CPU\n");
+        printf("3 - информация о GPU\n");
         printf("Выберите действие:");
         scanf_s("%d", &option);
         switch (option) {
             case Options::PAR_MUL_GPU: {
                 scan_matrix_dim(&matrix_dim);
 
-                size_t matrix_size = matrix_dim*matrix_dim;
+                int matrix_size = matrix_dim*matrix_dim;
                 size_t m_size = malloc_size<float>(matrix_size);
 
                 cpuA = new float[matrix_size] {};
                 cpuB = new float[matrix_size] {};
-                cpuC = new float[matrix_size] {};
+                cpuC = new float[matrix_size] {0.0f};
 
                 curandGenerateUniform(gen, cpuA, matrix_size);
                 curandGenerateUniform(gen, cpuB, matrix_size);
@@ -137,7 +225,7 @@ int main() {
                 cudaMalloc((void**)&a, m_size);
                 cudaMalloc((void**)&b, m_size);
                 cudaMalloc((void**)&c, m_size);
-                cudaMalloc((void**)&c_buffer, m_size);
+                cudaMalloc((void**)&buffer, m_size);
 
 #ifdef LAB_DEBUG
                 print_matrix(cpuA, matrix_dim);
@@ -145,7 +233,7 @@ int main() {
 #endif
 
                 cudaMemcpy(a, cpuA, m_size, cudaMemcpyHostToDevice);
-                cudaMemcpy(b, cpuB, m_size, cudaMemcpyHostToDevice);
+                cudaMemcpy(buffer, cpuB, m_size, cudaMemcpyHostToDevice);
 
                 // Initialize events
                 cudaEvent_t start, stop;
@@ -158,9 +246,12 @@ int main() {
                 // Record events
                 cudaEventRecord(start, nullptr);
 
-                cuda_mmul(&HANDLE, c_buffer, a, b, c,
+                transpose(&HANDLE, buffer, a, b, matrix_dim, matrix_dim, matrix_dim, matrix_dim);
+
+                /*cuda_mmul(&HANDLE, buffer, a, b, c,
                           matrix_dim, matrix_dim, matrix_dim,
-                          matrix_dim, matrix_dim, matrix_dim);
+                          matrix_dim, matrix_dim, matrix_dim);*/
+                cudaMmul<<<dim3(matrix_dim / BLOCK_SIZE, matrix_dim), BLOCK_SIZE>>>(a, b, c, matrix_dim, INTCAST(matrix_size));
 
                 cudaEventRecord(stop, nullptr);
 
@@ -184,7 +275,7 @@ int main() {
                 cudaFree(a);
                 cudaFree(b);
                 cudaFree(c);
-                cudaFree(c_buffer);
+                cudaFree(buffer);
 
                 delete[] cpuA;
                 delete[] cpuB;
@@ -220,6 +311,10 @@ int main() {
                 delete[] cpuB;
                 delete[] cpuC;
 
+                break;
+            }
+            case Options::DEVICE_INFO: {
+                showProperties();
                 break;
             }
             case Options::EXIT:{
